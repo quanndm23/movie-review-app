b12130b0bdcec4a28a8e194da29ddad219b210d4
tung1512 <tungypbn2001@gmail.com>
2025-12-22 22:22:43 +0700

    init project docs: README chore: config

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import MovieDetail from './pages/MovieDetail/MovieDetail';
+import Search from './pages/Search/Search';
+import Layout from './components/Layout/Layout';
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      {/* <div className="min-h-screen bg-imdb-dark pt-16"> */}
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetail />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Layout>
+
+      {/* </div> */}
+    </BrowserRouter>
+  );
+};
+
+export default App;
